4a979da195e0628983077abdeb46f2d4f8480f88
tonarerenato <tonarerenato@yahoo.com.br>
2020-03-11 16:04:41 -0300

    projeto criado

---
+++ src/main/java/renatospringboot/CursomcApplication.java
@@ -0,0 +1,13 @@
+package renatospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
