16b6ace20128d02cbbb60179cc79cb480cac8602
tonarerenato <tonarerenato@yahoo.com.br>
2020-03-12 02:24:26 -0300

    criando operacao de isntanciacao

--- src/main/java/renatospringboot/CursomcApplication.java
+++ src/main/java/renatospringboot/CursomcApplication.java
@@ -1,13 +1,33 @@
 package renatospringboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import renatospringboot.domain.Categoria;
+import renatospringboot.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+//CommandLineRunner permite um metodo auxiliar quando a aplicacao iniciar
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+@Override
+public void run(String... args) throws Exception {
+	Categoria cat1 = new Categoria(null, "Informáticaa");
+	Categoria cat2 = new Categoria(null, "Escritorioo");
+	
+	categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	
+}
 
 }
