580d79979e7d370a9eb531cf85e254d433623cee
tonarerenato <tonarerenato@yahoo.com.br>
2020-03-26 19:37:31 -0300

    criando profile de desenvolvimento

--- src/main/java/renatospringboot/config/DevConfig.java
+++ src/main/java/renatospringboot/config/DevConfig.java
@@ -3,6 +3,7 @@ package renatospringboot.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,14 +11,21 @@ import org.springframework.context.annotation.Profile;
 import renatospringboot.services.DBService;
 
 @Configuration
-@Profile("dev")
-public class TestConfig {
+@Profile("test")
+public class DevConfig {
 	
 	@Autowired
 	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	@Bean
 	public boolean instantiateDatabase () throws ParseException {
+		
+		if(!"create".contentEquals(strategy)){
+			return false;
+		}
 		dbService.instantiateTestDatabase();
 		
 		return true;
