f9aab363296712f737411ab0de78d39ee6d620c6
tonarerenato <tonarerenato@yahoo.com.br>
2020-04-01 13:54:56 -0300

    Refresh token

---
+++ src/main/java/renatospringboot/resources/AuthResource.java
@@ -0,0 +1,27 @@
+package renatospringboot.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import renatospringboot.security.JWTUtil;
+import renatospringboot.security.UserSS;
+import renatospringboot.services.UserService;
+
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+
+}
