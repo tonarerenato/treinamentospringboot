98aeeecc375c7a8a6040223bab4a9449d71819db
tonarerenato <tonarerenato@yahoo.com.br>
2020-04-02 11:04:27 -0300

    Enviando imagem via endpoint

--- src/main/java/renatospringboot/CursomcApplication.java
+++ src/main/java/renatospringboot/CursomcApplication.java
@@ -10,8 +10,8 @@ import renatospringboot.services.S3Service;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	@Autowired
-	private S3Service s3Service;
+//	@Autowired
+//	private S3Service s3Service;
 	
 
 	//CommandLineRunner permite um metodo auxiliar quando a aplicacao iniciar
@@ -21,7 +21,7 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Users\\Usuario\\code\\udemy-docker-mastery\\compose-sample-3\\html\\img\\portfolio\\06-full.jpg"); 
+		//s3Service.uploadFile("C:\\Users\\Usuario\\code\\udemy-docker-mastery\\compose-sample-3\\html\\img\\portfolio\\06-full.jpg"); 
 	}
 
 }
